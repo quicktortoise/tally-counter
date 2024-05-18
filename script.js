86bf09e96f3c03b15ae1ace7071feeea6538415a
Daniel Medrano Huerta <daniel@quicktortoise.com>
2024-05-18 08:04:50 -0600

    Several small fixes and improvements
    * Change indentation for style.css from tabs to spaces (2)
    * Use flex container for the body layout and the main layout
    * Prevent zooming on the page
    * Add a Copy button to copy the current value of the counter
    * Add a footer that contains a link to the Github repository
    * Re-wire the scripts to match the new interface
    * Use the same event listener for all the actions (document level).

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 (function () {
-  const counter = document.querySelector('.contador');
+  const counter = document.querySelector(".counter");
+  const btnCopy = document.getElementById("btn-copy");
+  const btnReset = document.getElementById("btn-reset");
+  const footerElements = [
+    document.querySelector("footer a"),
+    document.querySelector("footer img.lighting-light"),
+    document.querySelector("footer img.lighting-dark"),
+  ];
 
-  /* Cuenta actual */
+  // Cuenta actual
   let count = 0;
 
-  /* Capturar toque en la pantalla o clic en la página para aumentar la cuenta actual en 1 */
-  document.addEventListener('click', () => counter.innerText = ++count);
+  // Capturar toque en la pantalla, o clic en la página
+  document.addEventListener("click", (e) => {
+    // Para aumentar la cuenta actual en 1, excepto cuando el evento
+    // haya sido lanzado desde los botones.
+    if (
+      e.target !== btnCopy &&
+      e.target !== btnReset &&
+      footerElements.every((element) => e.target !== element)
+    )
+      ++count;
 
-  /* Capturar activación del botón "Restablecer" para establecer la cuenta actual en -1.
-  *  Al capturarse el evento de clic global (document), la cuenta actual será 0 */
-  document.querySelector('.boton-restablecer').addEventListener('click', () => count = -1);
+    // Para copiar el número actual al portapapeles.
+    if (e.target === btnCopy) navigator.clipboard.writeText(count);
 
-  counter.innerText = count;
+    // Para establecer la cuenta actual en -1.
+    if (e.target === btnReset) count = 0;
+
+    counter.innerText = count;
+  });
 })();
